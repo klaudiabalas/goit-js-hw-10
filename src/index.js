ca4e4a173c14382d0a41598291ec7ad4e861aa00
klaudiab <klaudiabalas1303@gmail.com>
2024-02-21 13:18:42 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
-import { fetchBreeds } from './cat.-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const selectBreed = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
 
-selectBreed.addEventListener('click', () => {
-  try {
-    fetchBreeds().then(data => renderSelect(data));
-  } catch (error) {
-    console.log(error);
-  }
-});
+try {
+  loader.classList.remove('hidden');
+  fetchBreeds().then(data => renderSelect(data));
+} catch (error) {
+  console.log(error);
+}
 
 function renderSelect(breeds) {
   const markup = breeds
     .map(({ id, name }) => {
-      return `<option value = "${id}">${name}</options>`;
+      return `<option value="${id}">${name}</option>`;
     })
     .join('');
-  selectBreed.insertAdjacentHTML('beforeend', markup);
+  breedSelect.insertAdjacentHTML('beforeend', markup);
+  loader.classList.add('hidden');
 }
 
 selectBreed.addEventListener('change', e => {
-  console.log(e.target.value);
+  loader.classList.remove('hidden');
+  fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
 });
+
+function renderCat(catData) {
+  const { url } = catData;
+  const { description, name, temperament } = catData.breeds[0];
+  catInfo.insertAdjacentHTML(
+    'beforeend',
+    `<div>
+          <h2>${name}</h2>
+          <img src="${url}" alt="${name}" />
+          <p>${description}</p>
+          <p><strong>Temperament:</strong> ${temperament}</p>
+      </div>`
+  );
+  loader.classList.add('hidden');
+}
