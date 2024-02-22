4c6b37d6caf0e48123972db37cde85eb9526a95c
klaudiab <klaudiabalas1303@gmail.com>
2024-02-22 12:10:39 +0100

    update

--- src/index.js
+++ src/index.js
@@ -12,13 +12,19 @@ try {
 }
 
 function renderSelect(breeds) {
-  const markup = breeds
-    .map(({ id, name }) => {
-      return `<option value="${id}">${name}</option>`;
-    })
-    .join('');
-  selectBreed.insertAdjacentHTML('beforeend', markup);
-  loader.classList.add('hidden');
+  if (typeof breeds == 'object') {
+    const markup = breeds
+      .map(({ id, name }) => {
+        return `<option value="${id}">${name}</option>`;
+      })
+      .join('');
+    selectBreed.insertAdjacentHTML('beforeend', markup);
+    loader.classList.add('hidden');
+  } else {
+    Notiflix.Report.failure('Error 404', data, 'OK');
+    loader.textContent = 'Error 404';
+    document.body.style.background = 'red';
+  }
 }
 
 selectBreed.addEventListener('change', e => {
@@ -29,14 +35,22 @@ selectBreed.addEventListener('change', e => {
 function renderCat(catData) {
   const { url } = catData;
   const { description, name, temperament } = catData.breeds[0];
+  catInfo.replaceChildren();
   catInfo.insertAdjacentHTML(
     'beforeend',
     `<div>
-          <h2>${name}</h2>
+          <h2 class="breed-name">${name}</h2>
           <img class="image" src="${url}" alt="${name}" />
+          <div class="container">
           <p>${description}</p>
           <p><strong>Temperament:</strong> ${temperament}</p>
+          </div>
       </div>`
   );
   loader.classList.add('hidden');
+  try {
+    fetchCatByBreed(changeSelect.target.value).then(data => catInfo(data));
+  } catch (error) {
+    console.log(error);
+  }
 }
